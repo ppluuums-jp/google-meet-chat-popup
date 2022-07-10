bed1c78c0666d1998f202ca72c749aaba0d11838
shim <104049111+ppluuums-jp@users.noreply.github.com>
2022-07-11 00:43:48 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# Google Meet Chat Pop-Up test
+
+Makes google meet chat pop up as a new window by just one click.
+
+When you press the generate button, a new window pops up as posted blow.
+
+<img width="200" src="https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/924548/91c774a3-d19a-4929-cd7c-c9312e073b17.png"><img width="100" src="https://user-images.githubusercontent.com/104049111/178147709-a2d8af18-c553-4149-8b32-1b4d1ed2c175.png">
+
+## Usage
+
+Follow the instruction and precautions down below.
+
+1. **Move to the google meet tab** (this enable the extension to get a tab id with the chrome API)
+2. **Open up the chat section**（this enable the extension to excute DOM manipulation）
+3. Open the extension by clicking the icon in the top right-hand corner of the screen and select "Google Meet Chat Popup"
+4. Press the generate button
+
+<img width="100" src="https://user-images.githubusercontent.com/104049111/178097192-64ec636b-b115-4ed7-8401-7497951eba18.png">
+
+※instruction2: Example of an open chat section
+
+## Installation
+
+Upcoming in chrome web store...
+
+## Developing locally
+
+1. Clone down the repository
+2. Open Tools → Extensions
+3. Check the "Developer Mode" option (if not already)
+4. Select "Load unpacked extension"
+5. Navigate to the recently cloned folder and click select
+
+## Contributer
+
+Developer: [@Fumi](https://github.com/KobayashiFumiaki)
---
+++ window.js
@@ -0,0 +1,220 @@
+"use strict";
+//Inicialize some variables
+let parent = document.getElementById("parent");
+let numberOfMessage_previous = 0;
+let count_block_previous = 0;
+let message_execute_count = 0;
+let back_on_track_flag = false;
+let i_tmp = 0;
+let j_tmp = 0;
+let no_message_flag = false;
+
+chrome.storage.local.get("meetTabId", async function(result){
+    let tabId = result.meetTabId;
+    await main(tabId);
+    setInterval(async () => {
+        await main(tabId);
+    }, 3000);
+});
+
+async function main(tabId) {
+    const count_block = await returnBlockCount(tabId);
+    const numberOfMessage_object = await returnNumberOfMessages(tabId);
+    const numberOfMessage = numberOfMessage_object.result;
+
+    //see if there are diffs of number of messages
+    if (numberOfMessage == 0 && !no_message_flag) {
+        setMessage("No message found.");
+        no_message_flag = true;
+    } else if (numberOfMessage_previous < numberOfMessage) {
+        //Execute
+        if (back_on_track_flag) {
+            message_execute_count = 0;
+            i_tmp = 0;
+            await getNewMessage("back on track.", count_block, tabId);
+            back_on_track_flag = false;
+        }
+        if (no_message_flag) {
+            parent.innerHTML = "";
+            no_message_flag = false;
+        }
+        //see if there are diffs of number of blocks
+        else if (count_block_previous == count_block.result) {
+            await getDiffs("no block diffs.", tabId);
+        } else {
+            await getNewMessage("There are new messages.", count_block, tabId);
+            // chrome.windows.create({ type: "popup" });
+        }
+
+        numberOfMessage_previous = numberOfMessage;
+        parent.scrollIntoView(false);
+    }
+}
+
+//Get a block count that contains names and times, messages
+function getBlockCount() {
+    return document.getElementsByClassName("GDhqjd").length;
+}
+
+async function returnBlockCount(tabId) {
+return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(
+    { target: { tabId: tabId }, func: getBlockCount },
+    (results) => {
+        if (results[0] === null) {
+        reject(new Error("Failed to parse meet."));
+        } else {
+        resolve(results[0]);
+        }
+    }
+    );
+});
+}
+
+//Get a number of messages
+function getNumberOfMessages() {
+return document.getElementsByClassName("oIy2qc").length;
+}
+
+async function returnNumberOfMessages(tabId) {
+return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(
+    { target: { tabId: tabId }, func: getNumberOfMessages },
+    (results) => {
+        if (results[0] === null) {
+        reject(new Error("Failed to parse meet."));
+        } else {
+        resolve(results[0]);
+        }
+    }
+    );
+});
+}
+
+//Get message counts which are included in each the blocks
+function getMessageCount(i) {
+if (i + 1 == 1) {
+    var content_content_length = document.querySelector(
+    "#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6 > div > div.Zmm6We"
+    ).childElementCount;
+} else {
+    var content_content_length = document.querySelector(
+    "#ow3 > div.T4LgNb > div > div:nth-child(10) > div.crqnQb > div.R3Gmyc.qwU8Me > div.WUFI9b > div.hWX4r > div > div.z38b6 > div:nth-child(" +
+        String(i + 1) +
+        ") > div.Zmm6We"
+    ).childElementCount;
+}
+return content_content_length;
+}
+
+async function returnMessageCount(tabId, i) {
+return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(
+    { target: { tabId: tabId }, func: getMessageCount, args: [i] },
+    (results) => {
+        if (results[0] === null) {
+        reject(new Error("Failed to parse meet."));
+        } else {
+        resolve(results[0]);
+        }
+    }
+    );
+});
+}
+
+//Get names and times from "Google Meet Chat"
+function parseNameAndTime(i) {
+return [
+    document.getElementsByClassName("YTbUzc")[i].textContent,
+    document.getElementsByClassName("MuzmKe")[i].textContent,
+];
+} 
+
+async function getNameAndTime(tabId, i) {
+return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(
+    { target: { tabId: tabId }, func: parseNameAndTime, args: [i] },
+    (results) => {
+        if (results[0] === null) {
+        reject(new Error("Failed to parse meet."));
+        } else {
+        resolve(results[0]);
+        }
+    }
+    );
+});
+}
+
+//Get Messages from "Google Meet Chat"
+function parseMessage(i) {
+return document.getElementsByClassName("oIy2qc")[i].textContent;
+} 
+
+async function getMessage(tabId, i) {
+return new Promise((resolve, reject) => {
+    chrome.scripting.executeScript(
+    { target: { tabId: tabId }, func: parseMessage, args: [i] },
+    (results) => {
+        if (results[0] === null) {
+        reject(new Error("Failed to parse meet."));
+        } else {
+        resolve(results[0]);
+        }
+    }
+    );
+});
+}
+
+//Execute when there are diffs of blocks
+async function getNewMessage(text, count_block, tabId) {
+//Count messages to get start point
+for (let i = i_tmp; i < count_block.result; i++) {
+    const count_message = await returnMessageCount(tabId, i);
+    const name_time_object = await getNameAndTime(tabId, i);
+    const name_time =
+    name_time_object.result[0] + " " + name_time_object.result[1];
+    setNameAndTime(name_time);
+
+    //Get messages for each name_times
+    for (let j = 0; j < count_message.result; j++) {
+    const get_message = await getMessage(tabId, message_execute_count);
+    const message = get_message.result;
+    setMessage(message);
+    message_execute_count++;
+    j_tmp = j + 1;
+    }
+    i_tmp = i + 1;
+}
+count_block_previous = count_block.result;
+}
+
+async function getDiffs(text, tabId) {
+    const count_message = await returnMessageCount(tabId, i_tmp - 1);
+
+    for (let j = j_tmp; j < count_message.result; j++) {
+        const get_message = await getMessage(tabId, message_execute_count);
+        const message = get_message.result;
+        setMessage(message);
+        message_execute_count++;
+        j_tmp = j + 1;
+    }
+}
+
+//Set names and times properly
+function setNameAndTime(name_time) {
+    let content_hr = document.createElement("hr");
+    let content_h6 = document.createElement("h6");
+    content_h6.className = "card-subtitle mb-2 text-muted";
+    content_h6.innerHTML = name_time;
+    parent.appendChild(content_hr);
+    parent.appendChild(content_h6);
+}
+
+//Set messages properly
+function setMessage(chat) {
+    let content_p = document.createElement("p");
+    content_p.className = "card-text";
+    content_p.innerHTML = chat;
+    parent.appendChild(content_p);
+}
+
